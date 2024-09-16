376e8de907156941d00b6fab8e56a24aec50e06d
rakeshcooper <rakeshmurali097@gmail.com>
2024-09-16 18:53:51 +0530

    today update

--- src/assets/Nestedarrayofobjects.jsx
+++ src/assets/Nestedarrayofobjects.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 
 function Nestedarrayofobject(){
-    const [cars,setCars] = useState([])
-
+    const [cars, setCars] = useState([])
+    const [carYear, setCarYear] = useState(new Date().getFullYear())
+    const [carMake, setCarMake] = useState("")
+    const [carModel, setCarModel] = useState("")
     return(
         <div>
             <ul>
+                <li>sample</li>
             </ul>
         </div>
     )

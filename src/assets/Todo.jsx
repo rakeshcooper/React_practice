c48b1b733a631a8bd15343dccf81f7b5d2ddc3a6
rakeshcooper <rakeshmurali097@gmail.com>
2024-09-22 00:00:36 +0530

    up

--- src/assets/Todo.jsx
+++ src/assets/Todo.jsx
@@ -1,18 +1,21 @@
 import { useState } from "react"
 
 function Todo(){
-    const [tasks, setTasks] = useState(["apple"])
+    const [tasks, setTasks] = useState(["apple","orange"])
+    
+    // setTasks(["pineapple"])
+    
     return(<div>
         <h2>Todo list added</h2>
         <input type="text" />
         <button>Add</button>
         <ul>
-            {tasks.map((task,index) => {
-                <li><span>{index}</span>
+            {tasks.map((task,index) => 
+                <li key={index}>
+                    <span>{task}</span>
                     <button>delete</button>
-                </li>
-                
-            })}
+                </li>    
+            )}
         </ul>
     </div>)
 }

b9714d8b7ea0c1e747d6df734c7d9507f38b2b5e
rakeshcooper <rakeshmurali097@gmail.com>
2024-09-21 21:53:58 +0530

    basic todo

--- src/assets/Todo.jsx
+++ src/assets/Todo.jsx
@@ -1,5 +1,20 @@
-function Todo(){
+import { useState } from "react"
 
+function Todo(){
+    const [tasks, setTasks] = useState(["apple"])
+    return(<div>
+        <h2>Todo list added</h2>
+        <input type="text" />
+        <button>Add</button>
+        <ul>
+            {tasks.map((task,index) => {
+                <li><span>{index}</span>
+                    <button>delete</button>
+                </li>
+                
+            })}
+        </ul>
+    </div>)
 }
 
 export default Todo

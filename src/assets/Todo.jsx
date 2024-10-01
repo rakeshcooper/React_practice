4dbdeaba1701f60bc83293cc0aa5d5d4df8bfed6
rakeshcooper <rakeshmurali097@gmail.com>
2024-10-01 23:56:35 +0530

    new update

--- src/assets/Todo.jsx
+++ src/assets/Todo.jsx
@@ -2,13 +2,20 @@ import { useState } from "react"
 
 function Todo(){
     const [tasks, setTasks] = useState(["apple","orange"])
+    const [newTask, setNewtask] = useState("")
     
-    // setTasks(["pineapple"])
+    function inputHandlerChange(event){
+        setNewtask(event.target.value)
+    }
+
+    function addTask(){
+        setTasks([newTask])
+    }
     
     return(<div>
         <h2>Todo list added</h2>
-        <input type="text" />
-        <button>Add</button>
+        <input onChange={(event) => inputHandlerChange(event)} type="text" />
+        <button onClick={addTask()}>Add</button>
         <ul>
             {tasks.map((task,index) => 
                 <li key={index}>

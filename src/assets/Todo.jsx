eb742cc6d91012c6948a9c019199a17963c6fe23
rakeshcooper <rakeshmurali097@gmail.com>
2024-10-02 01:53:57 +0530

    new up

--- src/assets/Todo.jsx
+++ src/assets/Todo.jsx
@@ -9,18 +9,25 @@ function Todo(){
     }
 
     function addTask(){
-        setTasks([newTask])
+        if(newTask.length != 0){
+            setTasks(t => [...t, newTask])
+            setNewtask("")
+        }
+    }
+
+    function removeArray(index){
+        setTasks(tasks.filter((_,i) => i !== index) )
     }
     
     return(<div>
         <h2>Todo list added</h2>
         <input onChange={(event) => inputHandlerChange(event)} type="text" />
-        <button onClick={addTask()}>Add</button>
+        <button onClick={() => addTask()}>Add</button>
         <ul>
             {tasks.map((task,index) => 
                 <li key={index}>
                     <span>{task}</span>
-                    <button>delete</button>
+                    <button onClick={()=> removeArray(index)}>delete</button>
                 </li>    
             )}
         </ul>

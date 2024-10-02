74a59d79d3776f21389b67fa44d72269c5bfb2f5
rakeshcooper <rakeshmurali097@gmail.com>
2024-10-03 00:58:04 +0530

    sec bday update

--- src/assets/Effect.jsx
+++ src/assets/Effect.jsx
@@ -1,7 +1,21 @@
+import { useState,useEffect } from "react"
+
+
 function Effect(){
- return(
+    const[count, setCount] = useState(0)
+
+    useEffect(() => {
+        document.title = `Count: ${count}`
+    })
+
+    function increaseHandler(){
+        setCount(count => count + 1)
+    }
+
+    return(
     <div>
-        <h1>Test</h1>
+        <p>Count: {count}</p>
+        <button onClick={increaseHandler}>Add count</button>
     </div>
  )
 }

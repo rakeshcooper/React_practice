89f8f8820548935114fedb07b8ef2cee057880ff
rakeshcooper <rakeshmurali097@gmail.com>
2024-10-04 21:58:36 +0530

    nu

--- src/assets/Effect.jsx
+++ src/assets/Effect.jsx
@@ -6,15 +6,20 @@ function Effect(){
 
     useEffect(() => {
         document.title = `Count: ${count}`
-    })
+    },[count])
 
     function increaseHandler(){
         setCount(count => count + 1)
     }
 
+    function decreaseHandler(){
+        setCount(count => count - 1)
+    }
+
     return(
     <div>
         <p>Count: {count}</p>
+        <button onClick={decreaseHandler}>Sub count</button>
         <button onClick={increaseHandler}>Add count</button>
     </div>
  )
